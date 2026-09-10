70d835d71487dd8a05f02e07ebb7e3d89d08236c
Jonathan Eguna <nath9neguna@gmail.com>
2026-09-10 11:14:46 +0800

    Initial portfolio

---
+++ src/app/app.routes.ts
@@ -0,0 +1,16 @@
+import { Routes } from '@angular/router';
+import { Hero } from './components/hero/hero';
+
+export const routes: Routes = [
+
+      {
+    path: '',
+    component: Hero
+  },
+
+  {
+    path: '**',
+    redirectTo: ''
+  }
+
+];
